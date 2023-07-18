8caa326be61d7b0d25e8f2d4c67aa124e8023eab
irishuang101 <26huangi49@stu.smuhsd.org>
2023-07-18 13:24:49 -0700

    Initial Commit

---
+++ ProdEase/ProdEaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProdEaseApp.swift
+//  ProdEase
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct ProdEaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
