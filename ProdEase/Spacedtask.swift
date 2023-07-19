00264833122bf47d51c439e7487c0acad8b76fd6
Kkiana22 <kianaylee@gmail.com>
2023-07-19 11:47:46 -0700

    .

---
+++ ProdEase/Spacedtask.swift
@@ -0,0 +1,20 @@
+//
+//  Spacedtask.swift
+//  ProdEase
+//
+//  Created by Scholar on 7/19/23.
+//
+
+import SwiftUI
+
+struct Spacedtask: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Spacedtask_Previews: PreviewProvider {
+    static var previews: some View {
+        Spacedtask()
+    }
+}
