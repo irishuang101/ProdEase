9d657fa22c1ee8331d8e1e94bea9ccc0dc23a4fe
irishuang101 <26huangi49@stu.smuhsd.org>
2023-07-19 13:24:12 -0700

    j

--- ProdEase/TimerApp.swift
+++ ProdEase/TimerApp.swift
@@ -6,44 +6,33 @@
 //
 
 import SwiftUI
-
-
 struct TimerApp: View {
-    @State private var timeRemaining = 60
-    @State private var timerIsRunning = false
-    var body: some View {
-        VStack {
-                Text("Timer: \(timeRemaining)")
-                    .font(.largeTitle)
-                    .padding()
-                    
-                if timerIsRunning {
-                    Button("Stop Timer") {
-                        timerIsRunning = false
-                        }
-                    } else {
-                        Button("Start Timer") {
-                            timerIsRunning = true
-                            startTimer()
-                        }
-                    }
-                }
-            }
-
-    private func startTimer() {
-            Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
-                if timeRemaining > 0 {
-                    timeRemaining -= 1
-                } else {
-                    timer.invalidate()
-                    timerIsRunning = false
-                }
-            }
+  @State private var timeRemaining = 60
+  @State private var timerIsRunning = false
+  let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+  var body: some View {
+    VStack {
+        Text("Timer: \(timeRemaining)")
+        .onReceive(timer) { _ in
+          if timeRemaining > 0 && timerIsRunning {
+            timeRemaining -= 1
+          } else {
+            timerIsRunning = false
+          }
         }
+      HStack{
+        Button("Start"){
+          timerIsRunning = true
+        }
+        Button("Reset"){
+          timeRemaining = 60
+        }
+      }
     }
-
+  }
+}
 struct TimerApp_Previews: PreviewProvider {
-    static var previews: some View {
-        TimerApp()
-    }
+  static var previews: some View {
+    TimerApp()
+  }
 }
